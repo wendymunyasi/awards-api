c9d10f1188bf80164ddf12b02a6f981b25cadee4
wendy munyasi <wendymunyasi@gmail.com>
2023-03-20 15:18:40 +0300

    Add routing for API endpoints using Django REST Framework

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('testPath/', admin.site.urls),
+    path('', include(router.urls)),
 ]
