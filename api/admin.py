162e7b1d174ec1c7e19729516fc7874db218f37a
wendy munyasi <wendymunyasi@gmail.com>
2023-03-20 15:36:06 +0300

    Register Project and Rating_Content models in admin.py and add models and Meta in models.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Rating_Content
+
+admin.site.register(Project)
+admin.site.register(Rating_Content)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
+
+
+class Project(models.Model):
+    """Class for project.
+    """
+    title = models.TextField(max_length=32)
+    description = models.TextField(max_length=360)
+
+
+class Rating_Content(models.Model):
+    """Class for rating.
+    """
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)])
+
+    class Meta:
+        """If a person tries to rate the same project, it will be rejected.
+        """
+        unique_together = (('user', 'project'))
+        index_together = (('user', 'project'))
