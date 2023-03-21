98ba8076a66acae5a5685aee355c613977486a37
wendy munyasi <wendymunyasi@gmail.com>
2023-03-21 15:52:11 +0300

    Add rate_project_content action to allow users to rate the content of a project

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from .models import Project, Rating_Content
 from .serializers import ProjectSerializer, RatingContentSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -9,6 +12,48 @@ class ProjectViewSet(viewsets.ModelViewSet):
     """
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    # The action decorator will route GET requests by default, but may also
+    # accept other HTTP methods by setting the methods argument.
+    # The decorator allows you to override any viewset-level configuration
+    # such as permission_classes, serializer_class, filter_backends.
+    # detail=True is 1 specific movie
+    # detail=False means list of movies
+    @action(detail=True, methods=['POST'])
+    def rate_project_content(self, request, pk=None):
+        """Rate the content of a project.
+        """
+        if 'stars' in request.data:
+            project = Project.objects.get(id=pk)
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
+
+            try:
+                # If rating exists for that project then update it
+                rating = Rating_Content.objects.get(
+                    user=user.id, project=project.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingContentSerializer(rating, many=False)
+                response = {
+                    'message': 'Rating updated',
+                    'result': serializer.data
+                }
+                return Response(response, status=status.HTTP_200_OK)
+            except Exception:
+                # If rating doesn't exist for that project then create it
+                rating = Rating_Content.objects.create(
+                    user=user, project=project, stars=stars)
+                serializer = RatingContentSerializer(rating, many=False)
+                response = {
+                    'message': 'Rating created',
+                    'result': serializer.data
+                }
+                return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message': 'You need to provide the ratings for stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingContentViewSet(viewsets.ModelViewSet):
