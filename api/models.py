769f8d08c7541b1d3b9d1cc92e74cfa9997d9189
wendy munyasi <wendymunyasi@gmail.com>
2023-03-22 14:50:15 +0300

    Add overall_rating method to calculate average of content, design and usability ratings

--- api/models.py
+++ api/models.py
@@ -72,6 +72,28 @@ class Project(models.Model):
         else:
             return 0
 
+    def overall_rating(self):
+        """Returns overall rating of a project
+        """
+        cont_sum = 0
+        desgn_sum = 0
+        usab_sum = 0
+        content_ratings = Rating_Content.objects.filter(project=self)
+        design_ratings = Rating_Design.objects.filter(project=self)
+        usability_ratings = Rating_Usability.objects.filter(project=self)
+        for rating in content_ratings:
+            cont_sum += rating.stars
+        for rating in design_ratings:
+            desgn_sum += rating.stars
+        for rating in usability_ratings:
+            usab_sum += rating.stars
+        ratings = cont_sum + desgn_sum + usab_sum
+        average = ratings / 3
+        if ratings > 0:
+            return math.floor(average)
+        else:
+            return 0
+
 
 class Rating_Content(models.Model):
     """Class for rating project according to its content.
