243efc735edb4c93c531b6a9a36f8065ebf66a42
wendy munyasi <wendymunyasi@gmail.com>
2023-03-22 15:12:23 +0300

    Add RatingDesignViewSet to router

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (ProjectViewSet, RatingContentViewSet,
+from .views import (ProjectViewSet, RatingContentViewSet, RatingDesignViewSet,
                     RatingUsabilityViewSet, UserViewSet)
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet)
 router.register('projects/ratings/content', RatingContentViewSet)
 router.register('projects/ratings/usability', RatingUsabilityViewSet)
+router.register('projects/ratings/design', RatingDesignViewSet)
 router.register('users', UserViewSet)
 
 urlpatterns = [
