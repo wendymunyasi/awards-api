b69d07251d0c6928c151668f2699d1e486f9c94b
wendy munyasi <wendymunyasi@gmail.com>
2023-03-22 11:25:39 +0300

    Correction to prev commit Add path to usability ratings

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import ProjectViewSet, RatingContentViewSet, UserViewSet
+from .views import ProjectViewSet, RatingContentViewSet, UserViewSet, \
+    RatingUsabilityViewSet
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet)
 router.register('projects/ratings/content', RatingContentViewSet)
-router.register('projects/ratings/usability', RatingContentViewSet)
+router.register('projects/ratings/usability', RatingUsabilityViewSet)
 router.register('users', UserViewSet)
 
 urlpatterns = [
